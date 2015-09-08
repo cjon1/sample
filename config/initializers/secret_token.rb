d82f005689606d5ece0642fd799d07c8f06b16ba
Colin Jones <colin.jones@phe.gov.uk>
2015-09-08 10:53:57 +0100

    Initial commit of /sample_app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'c40e9a4e363f36a39db67660aff283e855e2a7cfa27c3902b99ca6f7d7e3f18135ebbb8681b81b93786e701d560934e8c73b55441ed006abc31e9fa9f28dfa38'
